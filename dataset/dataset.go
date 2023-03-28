ba61e168aad6af854bbb72bde4f6a8aec448a6e1
Godwinh19 <houdjigodwin@gmail.com>
2023-03-28 17:31:40 +0100

    add gorgonia model

--- dataset/dataset.go
+++ dataset/dataset.go
@@ -1,12 +1,52 @@
 package dataset
 
-// DataSet represents a dataset of training examples
-type DataSet struct {
-	data []Example
-}
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
 
-// Example represents a single training example
-type Example struct {
-	Features []float64
-	Label    int
+	"gorgonia.org/tensor"
+)
+
+func LoadIrisDataset(filename string) (data, labels *tensor.Dense, err error) {
+	// Load data from file
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	rows, err := r.ReadAll()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Parse data and labels
+	numSamples := len(rows)
+	data = tensor.New(tensor.WithShape(numSamples, 4), tensor.WithBacking(make([]float64, numSamples*4)))
+	labels = tensor.New(tensor.WithShape(numSamples, 1), tensor.WithBacking(make([]int, numSamples)))
+
+	for i, row := range rows {
+		// Parse data
+		for j := 0; j < 4; j++ {
+			val, err := strconv.ParseFloat(row[j], 64)
+			if err != nil {
+				return nil, nil, err
+			}
+			data.SetAt(val, i, j)
+		}
+
+		// Parse label
+		switch row[4] {
+		case "Iris-setosa":
+			labels.SetAt(0, i, 0)
+		case "Iris-versicolor":
+			labels.SetAt(1, i, 0)
+		case "Iris-virginica":
+			labels.SetAt(2, i, 0)
+		}
+	}
+
+	return data, labels, nil
 }
--- model/model.go
+++ model/model.go
@@ -1,31 +1,95 @@
 package model
 
-import "federated-learning/dataset"
+import (
+	"encoding/json"
+	"federated-learning/dataset"
+	"fmt"
+	"gorgonia.org/gorgonia"
+	"gorgonia.org/tensor"
+	"log"
+	"os"
+)
 
-//Typically a model is a collection of layers
-// Each layer have its own parameters and gradients
-type LayerType struct {
-	Params []float64
-}
-type Model struct {
-	name string
-	Layer []LayerType
+type model struct {
+	W1, B1, W2, B2 *tensor.Dense
 }
 
-// initialize a new model
-func (m *Model) New() *Model {
-	return &Model{
-		name: "new-version",
+func Net() {
+	// Load Iris dataset
+	data, labels, err := dataset.LoadIrisDataset("dataset/iris.data")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Define neural network architecture
+	g := gorgonia.NewGraph()
+	x := gorgonia.NewMatrix(g, tensor.Float64, gorgonia.WithShape(data.Shape()[0], 4), gorgonia.WithName("x"))
+	y := gorgonia.NewMatrix(g, tensor.Float64, gorgonia.WithShape(data.Shape()[0], 1), gorgonia.WithName("y"))
+	w1 := gorgonia.NewMatrix(g, tensor.Float64, gorgonia.WithShape(4, 8), gorgonia.WithName("w1"))
+	//b1 := gorgonia.NewVector(g, tensor.Float64, gorgonia.WithShape(150, 8), gorgonia.WithName("b1"))
+	w2 := gorgonia.NewMatrix(g, tensor.Float64, gorgonia.WithShape(8, 1), gorgonia.WithName("w2"))
+	//b2 := gorgonia.NewVector(g, tensor.Float64, gorgonia.WithShape(1), gorgonia.WithName("b2"))
+
+	// Define neural network operations
+	hidden := gorgonia.Must(gorgonia.Mul(x, w1))
+	//hidden = gorgonia.Must(gorgonia.Add(hidden, b1))
+	hidden = gorgonia.Must(gorgonia.Rectify(hidden))
+	output := gorgonia.Must(gorgonia.Mul(hidden, w2))
+	//output = gorgonia.Must(gorgonia.Add(output, b2))
+
+	// Define loss function
+	cost := gorgonia.Must(gorgonia.Mean(gorgonia.Must(gorgonia.Square(gorgonia.Must(gorgonia.Sub(output, y))))))
+
+	// Define gradients
+	grads, err := gorgonia.Grad(cost, w1, w2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Define VM
+	machine := gorgonia.NewTapeMachine(g, gorgonia.BindDualValues(w1, w2))
+
+	// Train neural network
+	solver := gorgonia.NewAdamSolver(gorgonia.WithLearnRate(0.1))
+	epochs := 20
+	for i := 0; i < epochs; i++ {
+		numExamples := data.Shape()[0]
+		for i := 0; i < numExamples; i++ {
+			xT, err := data.Slice(gorgonia.S(i, i+1), gorgonia.S(0, 4))
+			yT, err := labels.Slice(gorgonia.S(i, i+1), gorgonia.S(0, 1))
+			_ = gorgonia.Let(x, xT)
+			_ = gorgonia.Let(y, yT)
+			_, err = gorgonia.Grad(cost, x, y)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if err := machine.RunAll(); err != nil {
+				fmt.Println(err)
+			}
+			if err := solver.Step(gorgonia.NodesToValueGrads(grads)); err != nil {
+				fmt.Println(err)
+			}
+			machine.Reset()
+		}
+		gorgonia.Let(x, data)
+		gorgonia.Let(y, labels)
+		if err := machine.RunAll(); err != nil {
+			fmt.Println(err)
+		}
+		costVal := cost.Value()
+		fmt.Printf("Epoch: %d, Cost: %f\n", i+1, costVal)
 	}
 }
 
-// trains the model on a given dataset
-func (m *Model) Train(dataSet *dataset.DataSet) {
-	// Implement training algorithm for the model
-}
-
-// evaluates the model on a given dataset and returns the accuracy
-func (m *Model) Evaluate(dataSet *dataset.DataSet) float64 {
-	// Implement evaluation algorithm for the model
-	return 0.0
+func saveModel(mod *model, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	enc := json.NewEncoder(file)
+	if err := enc.Encode(mod); err != nil {
+		return err
+	}
+	return nil
 }
